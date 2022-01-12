4bf998ccd0a64089f138d36cd88eeb9609064216
brandon82890 <brandon82890@gmail.com>
2022-01-12 13:30:23 -0500

    removed rust-tools wasn't working with rust-analyzer language server

--- nvim/lua/lsp-config/language-servers.lua
+++ nvim/lua/lsp-config/language-servers.lua
@@ -60,17 +60,5 @@ lsp_installer.on_server_ready(function(server)
 		on_attach = on_attach,
 		capabilities = capabilities,
 	}
-	if server.name == "rust_analyzer" then
-		-- Initialize the LSP via rust-tools instead
-		require("rust-tools").setup({
-			-- The "server" property provided in rust-tools setup function are the
-			-- settings rust-tools will provide to lspconfig during init.
-			-- We merge the necessary settings from nvim-lsp-installer (server:get_default_options())
-			-- with the user's own settings (default_opts).
-			server = vim.tbl_deep_extend("force", server:get_default_options(), default_opts),
-		})
-		server:attach_buffers()
-	else
-		server:setup(default_opts)
-	end
+	server:setup(default_opts)
 end)
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -159,11 +159,6 @@ _G.packer_plugins = {
     path = "/home/brandon/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
-  ["rust-tools.nvim"] = {
-    loaded = true,
-    path = "/home/brandon/.local/share/nvim/site/pack/packer/start/rust-tools.nvim",
-    url = "https://github.com/simrat39/rust-tools.nvim"
-  },
   ["telescope-fzf-native.nvim"] = {
     loaded = true,
     path = "/home/brandon/.local/share/nvim/site/pack/packer/start/telescope-fzf-native.nvim",
