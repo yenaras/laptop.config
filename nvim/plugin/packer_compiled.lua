93cb8da1cb80657221ce3c382900f8fd6d657b57
brandon82890 <brandon82890@gmail.com>
2022-01-12 22:41:36 -0500

    moved zsh to .config and added temporary screenshot keybind

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -44,8 +44,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/brandon/.cache/nvim/packer_hererocks/2.0.5/share/lua/5.1/?.lua;/home/brandon/.cache/nvim/packer_hererocks/2.0.5/share/lua/5.1/?/init.lua;/home/brandon/.cache/nvim/packer_hererocks/2.0.5/lib/luarocks/rocks-5.1/?.lua;/home/brandon/.cache/nvim/packer_hererocks/2.0.5/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/brandon/.cache/nvim/packer_hererocks/2.0.5/lib/lua/5.1/?.so"
+local package_path_str = "/home/brandon/.config/cache/nvim/packer_hererocks/2.0.5/share/lua/5.1/?.lua;/home/brandon/.config/cache/nvim/packer_hererocks/2.0.5/share/lua/5.1/?/init.lua;/home/brandon/.config/cache/nvim/packer_hererocks/2.0.5/lib/luarocks/rocks-5.1/?.lua;/home/brandon/.config/cache/nvim/packer_hererocks/2.0.5/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/brandon/.config/cache/nvim/packer_hererocks/2.0.5/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -71,107 +71,107 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   LuaSnip = {
     loaded = true,
-    path = "/home/brandon/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/home/brandon/.config/local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["barbar.nvim"] = {
     loaded = true,
-    path = "/home/brandon/.local/share/nvim/site/pack/packer/start/barbar.nvim",
+    path = "/home/brandon/.config/local/share/nvim/site/pack/packer/start/barbar.nvim",
     url = "https://github.com/romgrk/barbar.nvim"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/home/brandon/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/home/brandon/.config/local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   cmp_luasnip = {
     loaded = true,
-    path = "/home/brandon/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
+    path = "/home/brandon/.config/local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
   ["lspkind-nvim"] = {
     loaded = true,
-    path = "/home/brandon/.local/share/nvim/site/pack/packer/start/lspkind-nvim",
+    path = "/home/brandon/.config/local/share/nvim/site/pack/packer/start/lspkind-nvim",
     url = "https://github.com/onsails/lspkind-nvim"
   },
   ["lualine.nvim"] = {
     loaded = true,
-    path = "/home/brandon/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    path = "/home/brandon/.config/local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
   ["nightfox.nvim"] = {
     loaded = true,
-    path = "/home/brandon/.local/share/nvim/site/pack/packer/start/nightfox.nvim",
+    path = "/home/brandon/.config/local/share/nvim/site/pack/packer/start/nightfox.nvim",
     url = "https://github.com/EdenEast/nightfox.nvim"
   },
   ["null-ls.nvim"] = {
     loaded = true,
-    path = "/home/brandon/.local/share/nvim/site/pack/packer/start/null-ls.nvim",
+    path = "/home/brandon/.config/local/share/nvim/site/pack/packer/start/null-ls.nvim",
     url = "https://github.com/jose-elias-alvarez/null-ls.nvim"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/home/brandon/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/home/brandon/.config/local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lsp-installer"] = {
     loaded = true,
-    path = "/home/brandon/.local/share/nvim/site/pack/packer/start/nvim-lsp-installer",
+    path = "/home/brandon/.config/local/share/nvim/site/pack/packer/start/nvim-lsp-installer",
     url = "https://github.com/williamboman/nvim-lsp-installer"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/home/brandon/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/home/brandon/.config/local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-notify"] = {
     loaded = true,
-    path = "/home/brandon/.local/share/nvim/site/pack/packer/start/nvim-notify",
+    path = "/home/brandon/.config/local/share/nvim/site/pack/packer/start/nvim-notify",
     url = "https://github.com/rcarriga/nvim-notify"
   },
   ["nvim-tree.lua"] = {
     loaded = true,
-    path = "/home/brandon/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
+    path = "/home/brandon/.config/local/share/nvim/site/pack/packer/start/nvim-tree.lua",
     url = "https://github.com/kyazdani42/nvim-tree.lua"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/home/brandon/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/home/brandon/.config/local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-treesitter-refactor"] = {
     loaded = true,
-    path = "/home/brandon/.local/share/nvim/site/pack/packer/start/nvim-treesitter-refactor",
+    path = "/home/brandon/.config/local/share/nvim/site/pack/packer/start/nvim-treesitter-refactor",
     url = "https://github.com/nvim-treesitter/nvim-treesitter-refactor"
   },
   ["nvim-web-devicons"] = {
     loaded = true,
-    path = "/home/brandon/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    path = "/home/brandon/.config/local/share/nvim/site/pack/packer/start/nvim-web-devicons",
     url = "https://github.com/kyazdani42/nvim-web-devicons"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/home/brandon/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/home/brandon/.config/local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/home/brandon/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/home/brandon/.config/local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["telescope-fzf-native.nvim"] = {
     loaded = true,
-    path = "/home/brandon/.local/share/nvim/site/pack/packer/start/telescope-fzf-native.nvim",
+    path = "/home/brandon/.config/local/share/nvim/site/pack/packer/start/telescope-fzf-native.nvim",
     url = "https://github.com/nvim-telescope/telescope-fzf-native.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/home/brandon/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/home/brandon/.config/local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["vim-tmux-navigator"] = {
     loaded = true,
-    path = "/home/brandon/.local/share/nvim/site/pack/packer/start/vim-tmux-navigator",
+    path = "/home/brandon/.config/local/share/nvim/site/pack/packer/start/vim-tmux-navigator",
     url = "https://github.com/christoomey/vim-tmux-navigator"
   }
 }
---
+++ zsh/.zshrc
@@ -0,0 +1,27 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.config/zsh/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+# Lines configured by zsh-newuser-install
+HISTFILE=~/.config/zsh/.histfile
+HISTSIZE=10000
+SAVEHIST=10000
+setopt beep extendedglob nomatch notify
+bindkey -v
+# End of lines configured by zsh-newuser-install
+# The following lines were added by compinstall
+zstyle :compinstall filename '/home/brandon/.config/zsh/.zshrc'
+
+# source alias file
+source /$HOME/.config/zsh/aliases
+
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
+source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
+
+# To customize prompt, run `p10k configure` or edit ~/.config/zsh/.p10k.zsh.
+[[ ! -f ~/.config/zsh/.p10k.zsh ]] || source ~/.config/zsh/.p10k.zsh
